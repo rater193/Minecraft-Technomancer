9e7a99dcd8f5472deaa12568be95ddaff16f4fba
rater193-workstation <rater193@gmail.com>
2023-03-15 13:09:31 -0400

    Initial code commit

---
+++ src/main/java/net/rater193/technomancer/Technomancer.java
@@ -0,0 +1,48 @@
+package net.rater193.technomancer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+
+// The value here should match an entry in the META-INF/mods.toml file
+// CTRL + F6 LETS YOU SAFELY RENAME FILES, VARIABLES, AND REFERENCES
+@Mod(Technomancer.MOD_ID)
+public class Technomancer
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "technomancer";
+
+    public Technomancer()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+        }
+    }
+}
